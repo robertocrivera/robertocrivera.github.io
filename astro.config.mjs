69cd580f498d7752147243890f33eed62a09f0bf
robertocrivera <robertocriveraval@gmail.com>
2026-09-06 00:08:56 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://robertocrivera.github.io",
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
